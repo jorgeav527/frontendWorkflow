d133ce94ccc198307b7f55a2c712a446c6eb443e
jorgeav527 <jorgeav527@gmail.com>
2018-09-22 22:09:29 -0500

    Build dist files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 //========= VARIABLES ==========//
-const gulp      = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    sass        = require('gulp-sass'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    concat = require('gulp-concat'),
-    imagemin = require('gulp-imagemin'),
-    plumber = require('gulp-plumber');
+const gulp        = require('gulp'),
+    browserSync       = require('browser-sync').create(),
+    sass              = require('gulp-sass'),
+    sourcemaps        = require('gulp-sourcemaps'),
+    concat            = require('gulp-concat'),
+    imagemin          = require('gulp-imagemin'),
+    htmlmin           = require('gulp-htmlmin'),
+    uglyfly          = require('gulp-uglyfly'),
+    autoprefixer      = require('gulp-autoprefixer'),
+    cleanCss          = require('gulp-clean-css'),
+    plumber           = require('gulp-plumber');
 
 
 //========= PATHS ==========//
@@ -105,7 +109,7 @@ gulp.task('styles-temp-file', () => {
 //=========  FONTS TEMP FILES  ==========//
 // Move FONTS folder to temp/fonts
 gulp.task('fonts-fa-temp-file', () => {
-    return gulp.src('node_modules/font-awesome/fonts/*')
+    gulp.src('node_modules/font-awesome/fonts/*')
         .pipe(gulp.dest('temp/fonts'));
 });
 
@@ -132,7 +136,7 @@ gulp.task('scripts-temp-file', () => {
 //=========  IMG TEMP FILES  ==========//
 // Copy plumber mini .PNG .JPG .GIF .SVG folder to temp/img
 gulp.task('img-temp-file', () => {
-    return gulp.src('src/img/**/*.+(png|jpg|gif|svg)')
+    gulp.src('src/img/**/*.+(png|jpg|gif|svg)')
       .pipe(plumber())
       .pipe(imagemin([
         imagemin.gifsicle({interlaced: true}),
@@ -165,16 +169,94 @@ gulp.task('build-temp-files',
 // Set up BrowserSync and run a local server from temp
 gulp.task('serve', () => {
     browserSync.init({
-      server: "./temp"
+        server: "./temp"
     });
-  });
+});
 
 // WATCH TEMP FILES
 gulp.task('watch', () => {
     gulp.watch('src/*.html', ['html-temp-file']).on('change', browserSync.reload)
     gulp.watch('src/styles/**/*.scss', ['styles-bootstrap-fa-temp-file', 'styles-temp-file']).on('change', browserSync.reload)
     gulp.watch('src/scripts/*.js', ['scripts-temp-file']).on('change', browserSync.reload)
+    gulp.watch('src/img/**/*', ['img-temp-file']).on('change', browserSync.reload)
 });
+
+/*
+BUILD DIST FILES
+*/
+//=========  HTML DIST FILES  ==========//
+// Copy concat and minify  HTML files to dist
+gulp.task('html-dist-file', () => {
+    gulp.src('src/*.html')
+        .pipe(concat('index.html'))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+});
+
+//=========  STYLES DIST FILES  ==========//
+// Copy plumber compilate autoprefixer clearCss compilate MAIN.SCSS files to dist/styles
+gulp.task('styles-dist-file', () => {
+    gulp.src('temp/styles/main.css')
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('dist/styles'))
+});
+
+// Copy cleanCss NORMALIZE.CSS files to dist/styles
+gulp.task('styles-dist-normalize', () => {
+    gulp.src('temp/styles/normalize.css')
+        .pipe(cleanCss())
+		.pipe(gulp.dest('dist/styles'));
+});
+
+// Copy and cleanCss BOOTSTRAP.SCSS and FONT-AWESOME.SCSS files to dist/styles
+gulp.task('styles-bootstrap-fa-dist-file', () => {
+    gulp.src([
+        'temp/styles/bootstrap.css', 
+        'temp/styles/font-awesome.css'])
+            .pipe(cleanCss())
+            .pipe(gulp.dest('dist/styles'))
+});
+
+//=========  FONTS DIST FILES  ==========//
+// Move FONTS folder to dist/fonts
+gulp.task('fonts-fa-dist-file', () => {
+    gulp.src('temp/fonts/*')
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+//=========  SCRIPTS DIST FILES  ==========//
+// Copy uglyfly BOOTSTRAP.MIN.JS JQUERY.MIN.JS POPPER MAIN.JS folder to dist/scritps
+gulp.task('scripts-bootstrap-jquery-poppers-dist-file', () => {
+    gulp.src([
+          'temp/scripts/bootstrap.min.js',
+          'temp/scripts/jquery.min.js',
+          'temp/scripts/popper.min.js',
+          'temp/scripts/main.js'
+        ])
+        .pipe(uglyfly())
+        .pipe(gulp.dest('dist/scripts'))
+});
+
+//=========  IMG DSIT FILES  ==========//
+// Copy plumber mini .PNG .JPG .GIF .SVG folder to dist/img
+gulp.task('img-dist-file', () => {
+    gulp.src('temp/img/**/*.+(png|jpg|gif|svg)')
+      .pipe(gulp.dest('dist/img'));
+});
+
+// BUILD DIST FILES 
+gulp.task('build-dist-files', 
+    [
+        'html-dist-file',
+        'styles-dist-file',
+        'styles-dist-normalize',
+        'styles-bootstrap-fa-dist-file',
+        'fonts-fa-dist-file',
+        'scripts-bootstrap-jquery-poppers-dist-file',
+        'img-dist-file'
+    ]
+);
 
 // RUN NEW PROJECT
 gulp.task('default', 
@@ -182,5 +264,9 @@ gulp.task('default',
         'build-src-files',
         'build-temp-files',
         'serve',
-        'watch'
-    ]);
+        'watch',
+        'build-dist-files'
+    ]
+);
+
+
